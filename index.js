82bc7ff77b422351eccca699c07c6763bf7f0455
Isaac <isaactan310@gmail.com>
2024-07-01 12:31:03 +0800

    done 3.8

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const http = require('http')
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+morgan.token('body', (request) => JSON.stringify(request.body))
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
