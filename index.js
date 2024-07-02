dac518f701cabf489293a8fdd2258ee8ed24aeb8
Isaac <isaactan310@gmail.com>
2024-07-02 11:22:59 +0800

    updated phonebook backend

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http = require('http')
 const express = require('express')
 const morgan = require('morgan')
-const app = express()
+const cors = require('cors')
 
+const app = express()
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', (request) => JSON.stringify(request.body))
@@ -111,7 +113,7 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
